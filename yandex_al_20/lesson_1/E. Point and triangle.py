556581d83eb9ddcf87a96065f99f14f9f7f335ba
Nikolay Sobyanin <kolya-sob@yandex.ru>
2023-02-07 20:58:31 +0300

    E. Point and triangle

---
+++ yandex_al_20/lesson_1/E. Point and triangle.py
@@ -0,0 +1,22 @@
+# Link: https://contest.yandex.ru/contest/28730/problems/E/
+
+
+def search_point(d: int, x: int, y: int) -> int:
+	if (x >= 0) and (y >= 0) and (x + y <= d):
+		return 0
+	
+	vertices = [(0, 0), (d, 0), (0, d)]
+	tmp_length = float('inf')
+	near_vertex = None
+	for vertex, point_vertex in enumerate(vertices, 1):
+		length = ((point[0] - point_vertex[0])**2 + (point[1] - point_vertex[1])**2)**0.5
+		if length < tmp_length:
+			tmp_length = length
+			near_vertex = vertex
+	return near_vertex
+
+
+d = int(input())
+point = tuple(map(int, input().split()))
+print(search_point(d, *point))
+
