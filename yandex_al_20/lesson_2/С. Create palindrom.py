fb8dd0406d10389e26361b5ffda47aa6fb5c41c4
Nikolay Sobyanin <kolya-sob@yandex.ru>
2023-02-10 15:23:01 +0300

    C. Create palindrom

---
+++ yandex_al_20/lesson_2/С. Create palindrom.py
@@ -0,0 +1,32 @@
+def cost(s: str) -> int:
+	"""Algorithm complexity == O(N)"""
+	l = len(s)
+	if (l == 0) and (l == 1):
+		return 0
+	
+	ans = 0
+	if len(s) % 2 != 0:
+		center = l // 2
+		for i in range(1, l // 2 + 1):
+			if s[center - i] != s[center + i]:
+				ans += 1
+	else:
+		center_r = l // 2
+		center_l = center_r - 1
+		for i in range(0, l // 2):
+			if s[center_l - i] != s[center_r + i]:
+				ans += 1
+	return ans
+
+
+assert cost('') == 0
+assert cost('a') == 0
+assert cost('acbca') == 0
+assert cost('acca') == 0
+assert cost('acbcd') == 1
+assert cost('accv') == 1
+assert cost('abcde') == 2
+
+
+s = input()
+print(cost(s))
