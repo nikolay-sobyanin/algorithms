3520fc7671c72a15e3260c4cdde5b4712a153a6f
Nikolay Sobyanin <kolya-sob@yandex.ru>
2023-02-14 17:01:14 +0300

    Optimization lesson 3

--- yandex_al_20/lesson_3/D. Guess number.py
+++ yandex_al_20/lesson_3/D. Guess number.py
@@ -1,7 +1,7 @@
 def guess_numbers(file_name: str):
     with open(file_name, 'r') as file:
         n = int(file.readline().strip())
-        all_set = set(range(1, n + 1))
+        possible = set(range(1, n + 1))
 
         while True:
             ans_b = file.readline().strip()
@@ -10,11 +10,11 @@ def guess_numbers(file_name: str):
             guess_set = set(map(int, ans_b.split()))
             ans_a = file.readline().strip()
             if ans_a == 'YES':
-                all_set &= guess_set
+                possible.intersection_update(guess_set)
             else:
-                all_set -= guess_set
+                possible.difference_update(guess_set)
 
-    print(' '.join(map(str, sorted(all_set))))
+    print(' '.join(map(str, sorted(possible))))
 
 
 guess_numbers('input.txt')
